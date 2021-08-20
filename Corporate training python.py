1ffab2e5f31cf4ab0c2bb0e54d32f59c4c285482
Mahimahulale <74171966+Mahimahulale@users.noreply.github.com>
2021-08-20 12:59:47 +0530

    Add files via upload

---
+++ Corporate training python.py
@@ -0,0 +1,24 @@
+##my_data='Mahima'
+##print(my_data)
+##print(type(my_data))
+
+
+##my_data='''india is my country \
+##and i love my country '''
+##print(my_data)
+
+##my_path=r"c:\newfolder\test.txt"
+##print(my_path)
+
+##my_data="Indii is history"
+##output=my_data.rfind("i")
+##print(output)
+
+##my_data=["dhoni","kohli",7,17]
+##my_data[0]="Rohit"
+##print(my_data)
+
+
+
+
+
